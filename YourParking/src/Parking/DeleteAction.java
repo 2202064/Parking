e39938d16bc13777f0127c58f5ad70946dcc8b76
2202064 <114715342+2202064@users.noreply.github.com>
2023-11-07 10:46:08 +0900

    ういうｇｖｇヴぎｖ

---
+++ YourParking/src/Parking/DeleteAction.java
@@ -0,0 +1,31 @@
+package login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.User;
+import dao.UserDAO;
+import tool.Action;
+
+public class DeleteAction extends Action {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user != null) {
+            int userId = user.getUser_Id();
+
+            UserDAO dao = new UserDAO();
+            int rowsDeleted = dao.delete(userId);
+
+            if (rowsDeleted > 0) {
+                session.invalidate();
+                return "taikai_kakunin.jsp";
+            }
+        }
+        return "login-error.jsp";
+    }
+}
