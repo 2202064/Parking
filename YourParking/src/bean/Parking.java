2d61c722bc188950207e9aa4e7561b89e7ec6623
2202064 <114715342+2202064@users.noreply.github.com>
2023-11-09 08:26:54 +0900

    Parking.java を更新

---
+++ YourParking/src/bean/Parking.java
@@ -0,0 +1,186 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Parking implements Serializable {
+    private int parkingId;
+    private String parkingName;
+    private String prefectures;
+    private String municipalities;
+    private String street;
+    private Double parkHigh;
+    private Double parkWidth;
+    private Double parkLength;
+    private Double parkWeight;
+    private String parkSpace;
+    private String parkTime;
+    private String parkMoney;
+    private String parkPayment;
+    private String parkGenre;
+    private Double actualHeight;
+    private Double actualWidth;
+    private Double actualLength;
+    private Double actualWeight;
+    private Double xCoord;
+    private Double yCoord;
+
+    public int getParkingId() {
+        return parkingId;
+    }
+
+    public void setParkingId(int parkingId) {
+        this.parkingId = parkingId;
+    }
+
+    public String getParkingName() {
+        return parkingName;
+    }
+
+    public void setParkingName(String parkingName) {
+        this.parkingName = parkingName;
+    }
+
+    public String getPrefectures() {
+        return prefectures;
+    }
+
+    public void setPrefectures(String prefectures) {
+        this.prefectures = prefectures;
+    }
+
+    public String getMunicipalities() {
+        return municipalities;
+    }
+
+    public void setMunicipalities(String municipalities) {
+        this.municipalities = municipalities;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Double getParkHigh() {
+        return parkHigh;
+    }
+
+    public void setParkHigh(Double parkHigh) {
+        this.parkHigh = parkHigh;
+    }
+
+    public Double getParkWidth() {
+        return parkWidth;
+    }
+
+    public void setParkWidth(Double parkWidth) {
+        this.parkWidth = parkWidth;
+    }
+
+    public Double getParkLength() {
+        return parkLength;
+    }
+
+    public void setParkLength(Double parkLength) {
+        this.parkLength = parkLength;
+    }
+
+    public Double getParkWeight() {
+        return parkWeight;
+    }
+
+    public void setParkWeight(Double parkWeight) {
+        this.parkWeight = parkWeight;
+    }
+
+    public String getParkSpace() {
+        return parkSpace;
+    }
+
+    public void setParkSpace(String parkSpace) {
+        this.parkSpace = parkSpace;
+    }
+
+    public String getParkTime() {
+        return parkTime;
+    }
+
+    public void setParkTime(String parkTime) {
+        this.parkTime = parkTime;
+    }
+
+    public String getParkMoney() {
+        return parkMoney;
+    }
+
+    public void setParkMoney(String parkMoney) {
+        this.parkMoney = parkMoney;
+    }
+
+    public String getParkPayment() {
+        return parkPayment;
+    }
+
+    public void setParkPayment(String parkPayment) {
+        this.parkPayment = parkPayment;
+    }
+
+    public String getParkGenre() {
+        return parkGenre;
+    }
+
+    public void setParkGenre(String parkGenre) {
+        this.parkGenre = parkGenre;
+    }
+
+    public Double getActualHeight() {
+        return actualHeight;
+    }
+
+    public void setActualHeight(Double actualHeight) {
+        this.actualHeight = actualHeight;
+    }
+
+    public Double getActualWidth() {
+        return actualWidth;
+    }
+
+    public void setActualWidth(Double actualWidth) {
+        this.actualWidth = actualWidth;
+    }
+
+    public Double getActualLength() {
+        return actualLength;
+    }
+
+    public void setActualLength(Double actualLength) {
+        this.actualLength = actualLength;
+    }
+
+    public Double getActualWeight() {
+        return actualWeight;
+    }
+
+    public void setActualWeight(Double actualWeight) {
+        this.actualWeight = actualWeight;
+    }
+
+    public Double getXCoord() {
+        return xCoord;
+    }
+
+    public void setXCoord(Double xCoord) {
+        this.xCoord = xCoord;
+    }
+
+    public Double getYCoord() {
+        return yCoord;
+    }
+
+    public void setYCoord(Double yCoord) {
+        this.yCoord = yCoord;
+    }
+}
