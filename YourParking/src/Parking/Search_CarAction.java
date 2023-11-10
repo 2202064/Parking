5729e692d83df8a1a381b307a10610e0bbc22392
exusial1224 <119024832+exusial1224@users.noreply.github.com>
2023-11-10 12:35:12 +0900

    Add files via upload

---
+++ YourParking/src/Parking/Search_CarAction.java
@@ -0,0 +1,33 @@
+package YourParking;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Car;
+import bean.User;
+import dao.CarDAO;
+import tool.Action;
+
+public class Search_CarAction extends Action {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        int user_Id = user.getUser_Id();
+
+        CarDAO dao = new CarDAO();
+        List<Car> cars = dao.Search(user_Id);
+        session.setAttribute("cars", cars);
+
+
+            if (cars != null) {
+                return "ownedcar_in.jsp";
+            }
+        return "login-error.jsp";
+    }
+}
