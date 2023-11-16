3709273994ab465682178221a0b06e78afedf95c
exusial1224 <119024832+exusial1224@users.noreply.github.com>
2023-11-16 12:21:59 +0900

    Add files via upload

---
+++ YourParking/src/car/AddCarAction.java
@@ -0,0 +1,52 @@
+package car;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Car;
+import bean.User;
+import dao.CarDAO;
+import tool.Action;
+
+public class AddCarAction extends Action {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("user");
+
+
+            if (user != null) {
+            	Integer user_id = user.getUser_id();
+                Car car = new Car();
+                car.setCar_name(request.getParameter("CAR_NAME"));
+                car.setCar_high(Double.parseDouble(request.getParameter("HIGH")));
+                car.setCar_width(Double.parseDouble(request.getParameter("WIDTH")));
+                car.setCar_length(Double.parseDouble(request.getParameter("LENGTH")));
+                car.setGround_height(Double.parseDouble(request.getParameter("GROUND_HEIGHT")));
+                car.setCar_weight(Double.parseDouble(request.getParameter("WEIGHT")));
+                car.setUser_id(user_id);
+
+                int result = new CarDAO().insert(car);
+                session.setAttribute("car", car);
+
+                if(result > 0){
+                	request.setAttribute("suc_message", "追加しました");
+                	return "addcar.jsp";
+                }else{
+                	request.setAttribute("fai_message", "失敗しました");
+                	return "addcar.jsp";
+                }
+            } else {
+                return "login_error2.jsp";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error.jsp";
+        }
+    }
+}
---
+++ YourParking/src/car/UpdCarAction.java
@@ -0,0 +1,57 @@
+package car;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Car;
+import bean.User;
+import dao.CarDAO;
+import tool.Action;
+
+public class UpdCarAction extends Action {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+        try {
+
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("user");
+
+
+            if (user != null) {
+                Car car = new Car();
+
+                car.setCar_id(Integer.parseInt(request.getParameter("car_id")));
+                car.setCar_name(request.getParameter("car_name"));
+                car.setCar_high(Double.parseDouble(request.getParameter("car_high")));
+                car.setCar_width(Double.parseDouble(request.getParameter("car_width")));
+                car.setCar_length(Double.parseDouble(request.getParameter("car_length")));
+                car.setGround_height(Double.parseDouble(request.getParameter("ground_height")));
+                car.setCar_weight(Double.parseDouble(request.getParameter("car_weight")));
+
+                int result = new CarDAO().update(car);
+
+                if(result > 0){
+                	request.setAttribute("suc_message", "更新しました");
+                    int user_id = user.getUser_id();
+
+                    CarDAO dao = new CarDAO();
+                    List<Car> cars = dao.search(user_id);
+                    session.setAttribute("cars", cars);
+                	return "upd_car.jsp";
+                }else{
+                	request.setAttribute("fai_message", "更新できませんでした");
+                	return "upd_car.jsp";
+                }
+            } else {
+                return "../error/login_error2.jsp";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "../error/error.jsp";
+        }
+    }
+}
