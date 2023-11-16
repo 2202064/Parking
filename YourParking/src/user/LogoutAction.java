214c9f219add13da65fa76639f40e13b8533b316
exusial1224 <119024832+exusial1224@users.noreply.github.com>
2023-11-16 12:25:04 +0900

    Add files via upload

---
+++ YourParking/src/user/LogoutAction.java
@@ -0,0 +1,24 @@
+package user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tool.Action;
+
+public class LogoutAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession();
+
+
+		if (session.getAttribute("user")!=null) {
+			session.removeAttribute("user");
+			return "logout_out.jsp";
+		}
+
+		return "../error/logout_error.jsp";
+	}
+}
