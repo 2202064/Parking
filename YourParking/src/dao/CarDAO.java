73d0e0c80329db062d9d2a70511cf5071c76fa3a
haruto-oota <148940843+haruto-oota@users.noreply.github.com>
2024-01-16 14:53:09 +0900

    Add files via upload

--- YourParking/src/dao/CarDAO.java
+++ YourParking/src/dao/CarDAO.java
@@ -10,7 +10,7 @@ import bean.Car;
 
 public class CarDAO extends DAO {
 
-	// 保存者取得
+	// 保存車取得
 	public List<Car> search(int user_id) throws Exception {
 	    List<Car> cars = new ArrayList<>();
 
@@ -79,6 +79,23 @@ public class CarDAO extends DAO {
         }
     }
 
+    public int insertplus(Car car) throws Exception {
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement("INSERT INTO CAR (CAR_ID, USER_ID, CAR_NAME, CAR_HIGH, CAR_WIDTH, CAR_LENGTH, CAR_WEIGHT, GROUND_HEIGHT, FLAG_USE) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+
+        	st.setInt(1, car.getUser_id());
+            st.setString(2, car.getCar_name());
+            st.setDouble(3, car.getCar_high());
+            st.setDouble(4, car.getCar_width());
+            st.setDouble(5, car.getCar_length());
+            st.setDouble(6, car.getCar_weight());
+            st.setDouble(7, car.getGround_height());
+            st.setInt(8, car.getFlag_use());
+
+            return st.executeUpdate();
+        }
+    }
+
     public int update(Car car) throws Exception {
         try (Connection con = getConnection();
              PreparedStatement st = con.prepareStatement("UPDATE CAR SET CAR_NAME=?, CAR_HIGH=?, CAR_WIDTH=?, CAR_LENGTH=?, CAR_WEIGHT=?, GROUND_HEIGHT=? WHERE CAR_ID = ? " )) {
@@ -120,10 +137,33 @@ public class CarDAO extends DAO {
     public int change_use(int user_id, int car_id) throws Exception {
     	Connection con = getConnection();
 
-    	PreparedStatement st = con.prepareStatement("UPDATE CAR SET FLAG_USE=1 WHERE USER_ID = ? AND CAR_ID = ?");
+    	PreparedStatement st = con.prepareStatement("UPDATE CAR SET FLAG_USE = 1 WHERE USER_ID = ? AND CAR_ID = ?");
     	st.setInt(1, user_id);
     	st.setInt(2, car_id);
 
+		return st.executeUpdate();
+    }
+
+    public int distinction(int user_id) throws Exception {
+
+    	Connection con = getConnection();
+
+    	PreparedStatement st = con.prepareStatement("SELECT COUNT(*) FROM CAR WHERE USER_ID = ?");
+    	st.setInt(1, user_id);
+
+    	ResultSet rs = st.executeQuery();
+    	if (rs.next()) {
+    		return rs.getInt(1);
+    	} else {
+    		return 0;
+    	}
+    }
+
+    public int only(int user_id) throws Exception {
+    	Connection con = getConnection();
+
+    	PreparedStatement st = con.prepareStatement("UPDATE CAR SET FLAG_USE = 1 WHERE USER_ID = ?");
+    	st.setInt(1, user_id);
 		return st.executeUpdate();
 
     }
