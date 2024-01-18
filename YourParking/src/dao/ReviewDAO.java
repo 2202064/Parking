b2e933e7d8f15cc0d74b5092b5efbd6c296d1d30
haruto-oota <148940843+haruto-oota@users.noreply.github.com>
2024-01-18 09:50:36 +0900

    Add files via upload

--- YourParking/src/dao/ReviewDAO.java
+++ YourParking/src/dao/ReviewDAO.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import bean.Review;
 
@@ -22,6 +25,35 @@ public class ReviewDAO extends DAO {
             return st.executeUpdate();
         }
     }
+
+    public List<Review> search(int parking_id) throws Exception {
+		List<Review> review = new ArrayList<>();
+
+		try (Connection con1 = getConnection();
+			PreparedStatement st1 = con1.prepareStatement("SELECT * FROM REVIEW WHERE PARKING_ID = ?")) {
+			st1.setInt(1, parking_id); // プレースホルダに値を設定
+
+	        try (ResultSet rs1 = st1.executeQuery()) {
+	            while (rs1.next()) {
+	                int user_id = rs1.getInt("user_id");
+
+	                try (Connection con2 = getConnection();
+	           	         PreparedStatement st2 = con2.prepareStatement("SELECT * FROM User WHERE USER_ID = ?")) {
+	           	        st2.setInt(1, user_id); // プレースホルダに値を設定
+
+	           	        try (ResultSet rs2 = st2.executeQuery()) {
+	           	            while (rs2.next()) {
+	           	            	Review rev = new Review();
+	           	            	rev.setUser_name(rs2.getString("user_name"));
+	           	            	rev.setComment(rs1.getString("comment"));
+	           	            }
+	           	        }
+	                }
+	            }
+	        }
+	    }
+		return review;
+	}
 
 
 }
